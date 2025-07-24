48846e50ed35a5bc46c448550333cfdffe5967d1
Henry Tan <htan0705@gmail.com>
2025-07-24 00:47:21 -0400

    ADD update API endpoint for issue - PUT

--- src/app/api/issues/[id]/route.ts
+++ src/app/api/issues/[id]/route.ts
@@ -1,5 +1,5 @@
 import { NextRequest, NextResponse } from "next/server";
-// import issueSchema from "../schema";
+import issueSchema from "../schema";
 import { prisma } from "@/../prisma/client";
 
 export async function GET(
@@ -16,4 +16,35 @@ export async function GET(
   return NextResponse.json(issue);
 }
 
-export async function PUT(request: NextRequest) {}
+export async function PUT(
+  request: NextRequest,
+  { params }: { params: { id: string } }
+) {
+  const body = await request.json();
+
+  const issue = await prisma.issue.findUnique({
+    where: { id: Number(params.id) },
+  });
+
+  if (!issue)
+    return NextResponse.json({ error: "Issue not found" }, { status: 404 });
+
+  const validate = issueSchema.safeParse(body);
+
+  if (!validate.success)
+    return NextResponse.json(validate.error.issues, { status: 400 });
+
+  const updatedIssue = await prisma.issue.update({
+    where: {
+      id: Number(params.id),
+    },
+    data: {
+      title: body.title,
+      description: body.description,
+    },
+  });
+
+  return NextResponse.json(updatedIssue, { status: 200 });
+}
+
+export async function DELETE(request: NextRequest) {}
