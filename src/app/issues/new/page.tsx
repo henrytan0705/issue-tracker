04a20d0df11f44542e19f587c9084cfcd652e93d
Henry Tan <htan0705@gmail.com>
2025-07-25 17:26:04 -0400

    Add error field for form

--- src/app/issues/new/page.tsx
+++ src/app/issues/new/page.tsx
@@ -1,52 +1,65 @@
 "use client";
 
-import { Button, TextField } from "@radix-ui/themes";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+import { Button, Callout, TextField } from "@radix-ui/themes";
 import SimpleMDE from "react-simplemde-editor";
 import "easymde/dist/easymde.min.css";
 import { useForm, Controller } from "react-hook-form";
 import axios from "axios";
-import { useRouter } from "next/navigation";
+import { MdError } from "react-icons/md";
+
 interface IssueForm {
   title: string;
   description: string;
 }
 
 const NewIssuePage = () => {
-  const { register, control, handleSubmit } = useForm<IssueForm>();
   const router = useRouter();
+  const { register, control, handleSubmit } = useForm<IssueForm>();
+  const [error, setError] = useState("");
 
   const handleFormSubmission = async (data: IssueForm) => {
     try {
-      const response = await axios.post("/api/issues", data);
-    } catch (err) {
-      console.error(err);
+      await axios.post("/api/issues", data);
+      router.push("/issues");
+    } catch (error) {
+      console.error(error);
+      setError("An error occured.");
     }
-
-    router.push("/issues");
   };
 
   return (
-    <form
-      className="max-w-xl space-y-3"
-      onSubmit={handleSubmit((data) => handleFormSubmission(data))}
-    >
-      <h1>New Issue</h1>
+    <div className="max-w-xl">
+      {error && (
+        <Callout.Root color="red" role="alert" className="mb-5">
+          <Callout.Icon>
+            <MdError />
+          </Callout.Icon>
+          <Callout.Text>{error}</Callout.Text>
+        </Callout.Root>
+      )}
 
-      <TextField.Root
-        placeholder="Title"
-        {...register("title")}
-      ></TextField.Root>
+      <form
+        className="space-y-3"
+        onSubmit={handleSubmit((data) => handleFormSubmission(data))}
+      >
+        <TextField.Root
+          placeholder="Title"
+          {...register("title")}
+        ></TextField.Root>
 
-      <Controller
-        name="description"
-        control={control}
-        render={({ field }) => (
-          <SimpleMDE placeholder="Description…" {...field} />
-        )}
-      />
+        <Controller
+          name="description"
+          control={control}
+          render={({ field }) => (
+            <SimpleMDE placeholder="Description…" {...field} />
+          )}
+        />
 
-      <Button type="submit">Create Issue</Button>
-    </form>
+        <Button type="submit">Create Issue</Button>
+      </form>
+    </div>
   );
 };
 
