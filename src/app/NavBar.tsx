0e1dcf6c57f2f314d0d2dd3f30e49e22983b1e2f
Henry Tan <htan0705@gmail.com>
2025-07-31 17:00:22 -0400

    Add Navbar dropdown - Display user avatar & email - Provide Link to logout url

--- src/app/NavBar.tsx
+++ src/app/NavBar.tsx
@@ -6,7 +6,15 @@ import { GiRadarSweep } from "react-icons/gi";
 import { usePathname } from "next/navigation";
 import classNames from "classnames";
 import { useSession } from "next-auth/react";
-import { Box } from "@radix-ui/themes";
+import {
+  Avatar,
+  Box,
+  Button,
+  Container,
+  DropdownMenu,
+  Flex,
+  Text,
+} from "@radix-ui/themes";
 
 const NavBar = () => {
   const pathname = usePathname();
@@ -18,35 +26,61 @@ const NavBar = () => {
   ];
 
   return (
-    <nav className="flex space-x-6 border-b mb-5 px-5 h-14 items-center">
-      <Link href="/">
-        <GiRadarSweep />
-      </Link>
-
-      <ul className="flex space-x-6">
-        {links.map(({ label, href }) => (
-          <li key={href}>
-            <Link
-              className={classNames({
-                "text-gray-500": pathname !== href,
-                "text-gray-900": pathname === href,
-                "hover:text-gray-800 transition-colors": true,
-              })}
-              href={href}
-            >
-              {label}
+    <nav className="border-b mb-5 px-5 py-3">
+      <Container>
+        <Flex justify="between">
+          <Flex align="center" gap="3">
+            <Link href="/">
+              <GiRadarSweep />
             </Link>
-          </li>
-        ))}
-      </ul>
 
-      <Box>
-        {status === "authenticated" ? (
-          <Link href="/api/auth/signout">Log Out</Link>
-        ) : (
-          <Link href="/api/auth/signin">Log In</Link>
-        )}
-      </Box>
+            <ul className="flex space-x-6">
+              {links.map(({ label, href }) => (
+                <li key={href}>
+                  <Link
+                    className={classNames({
+                      "text-gray-500": pathname !== href,
+                      "text-gray-900": pathname === href,
+                      "hover:text-gray-800 transition-colors": true,
+                    })}
+                    href={href}
+                  >
+                    {label}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </Flex>
+
+          <Box>
+            {status === "authenticated" ? (
+              <DropdownMenu.Root>
+                <DropdownMenu.Trigger>
+                  <Avatar
+                    src={session.user!.image!}
+                    fallback="?"
+                    size="2"
+                    radius="full"
+                    className="cursor-pointer"
+                    referrerPolicy="no-referrer"
+                  />
+                </DropdownMenu.Trigger>
+
+                <DropdownMenu.Content>
+                  <DropdownMenu.Label>
+                    <Text size="2">{session.user?.email}</Text>
+                  </DropdownMenu.Label>
+                  <DropdownMenu.Item>
+                    <Link href="/api/auth/signout">Log Out</Link>
+                  </DropdownMenu.Item>
+                </DropdownMenu.Content>
+              </DropdownMenu.Root>
+            ) : (
+              <Link href="/api/auth/signin">Log In</Link>
+            )}
+          </Box>
+        </Flex>
+      </Container>
     </nav>
   );
 };
