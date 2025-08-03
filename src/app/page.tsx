0693daa1924793d088242da518f4450ef7082c5c
Henry Tan <htan0705@gmail.com>
2025-08-03 00:31:01 -0400

    Implement pagination

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,18 @@
-export default function Home() {
-  return <div> This is the home page.</div>;
+import Pagination from "@/app/components/Pagination";
+
+interface Props {
+  searchParams: { page: string };
+}
+
+export default function Home({ searchParams }: Props) {
+  return (
+    <>
+      <h1>Home Page</h1>
+      <Pagination
+        itemCount={100}
+        pageSize={10}
+        currentPage={Number(searchParams.page) || 1}
+      />
+    </>
+  );
 }
