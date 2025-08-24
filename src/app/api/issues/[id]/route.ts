3844d98007f72456137abf6efb78157b9a81d0c5
Henry Tan <htan0705@gmail.com>
2025-08-24 17:08:58 -0400

    Update request signature

--- src/app/api/issues/[id]/route.ts
+++ src/app/api/issues/[id]/route.ts
@@ -5,7 +5,7 @@ import { getServerSession } from "next-auth";
 import authOptions from "../../auth/[...nextauth]/authOptions";
 
 export async function GET(
-  request: NextRequest,
+  _request: NextRequest,
   { params }: { params: Promise<{ id: string }> }
 ) {
   try {
